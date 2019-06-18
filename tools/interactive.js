fb399af0a6b839b82663277c60868c64944127b3
Mathieu Dombrock <matdombrock@gmail.com>
2019-06-18 06:05:44 -0700

    Update interactive.js

--- tools/interactive.js
+++ tools/interactive.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
 global.lifx = require('../LIFX-Wrapper'); 
 global.lifx.setToken(process.env.API_TOKEN);
-console.log("Set TOKEN as: "+process.env.API_TOKEN);
 console.log(global.lifx);
+console.log("Set TOKEN as: "+process.env.API_TOKEN);
