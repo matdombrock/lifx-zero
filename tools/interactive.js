801773a689ec00e06c27a78cddda16c5609b1eb2
Mathieu Dombrock <matdombrock@gmail.com>
2019-06-18 06:21:10 -0700

    Change main script name

--- tools/interactive.js
+++ tools/interactive.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-global.lifx = require('../LIFX-Wrapper'); 
+global.lifx = require('../lifxWrapper'); 
 global.lifx.setToken(process.env.API_TOKEN);
 console.log(global.lifx);
 console.log("Set TOKEN as: "+process.env.API_TOKEN);
